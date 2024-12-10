ec0a2385bea9b80ffbb350daf8abe628b64a9b90
ibrahim kurut <ibrahim.kurut06@gmail.com>
2024-12-10 22:11:33 +0300

    show the count of likes for each post

--- backend/blog/serializers.py
+++ backend/blog/serializers.py
@@ -31,6 +31,11 @@ class PostSerializer(serializers.ModelSerializer):
     # View all comments related to post
     comments = CommentSerializer(many=True, read_only=True)
 
+    # show the count of likes for each post
+    likes_count = serializers.SerializerMethodField()
+    def get_likes_count(self, obj):
+        return obj.likes.count()
+
 
 class LikesSerializer(serializers.ModelSerializer):
     class Meta:
